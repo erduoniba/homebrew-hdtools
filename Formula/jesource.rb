797d262d6916c6bd7c5a14023dc124fa38cd8a3f
denglibing <denglibing@hd.com>
2025-07-25 11:46:56 +0800

    fix: 更新二进制路径

--- Formula/jesource.rb
+++ Formula/jesource.rb
@@ -7,35 +7,9 @@ class Jesource < Formula
   version "1.0.0"
   url "https://github.com/erduoniba/homebrewdist/releases/download/1.0.0/jesource-v1.0.0-Darwin-arm64-binary-20250725_105414.tar.gz"
   sha256 "a28dbb383434acdb5700f02b6e9050d8151ce71a3e07beb1191bd49038ed770c"
-  license ""
-
-  # depends_on "cmake" => :build
-
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    bin.install "jesource"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test jesource`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    bin.install "jesource/jesource" "jesource"
   end
 
     def caveats
