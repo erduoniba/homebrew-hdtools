1b520c0b0f4c21d238378abff326af832f5ec7ab
denglibing <denglibing@hd.com>
2025-07-25 12:11:18 +0800

    feat: 修复 jesource formula 配置

--- Formula/jesource.rb
+++ Formula/jesource.rb
@@ -1,18 +1,44 @@
 class Jesource < Formula
   desc "LLDB source code mapping tool for iOS development"
-  homepage "https://github.com/erduoniba/homebrewdist"
+  homepage "https://github.com/erduoniba/homebrew-hdtools"
   version "1.0.0"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/erduoniba/homebrewdist/releases/download/1.0.0/jesource-v1.0.0-Darwin-arm64-binary-20250725_105414.tar.gz"
+      url "https://github.com/erduoniba/homebrew-hdtools/raw/main/jesource/jesource-v1.0.0-Darwin-arm64-binary-20250725_105414.tar.gz"
       sha256 "a28dbb383434acdb5700f02b6e9050d8151ce71a3e07beb1191bd49038ed770c"
     end
   end
 
   def install
-    # 把解压出来的 jesource/jesource 文件，复制到 Homebrew 的 bin 目录，并且重命名为 jesource。
-    bin.install "jesource/jesource" => "jesource"
+    # 处理可能的目录结构
+    if File.exist?("jesource/jesource")
+      bin.install "jesource/jesource" => "jesource"
+    elsif File.exist?("jesource")
+      bin.install "jesource" => "jesource"
+    else
+      # 查找可执行文件
+      jesource_file = Dir.glob("**/jesource").find { |f| File.executable?(f) }
+      if jesource_file
+        bin.install jesource_file => "jesource"
+      else
+        odie "Could not find jesource executable"
+      end
+    end
+    
+    # 安装文档
+    if File.exist?("jesource/README.md")
+      doc.install "jesource/README.md"
+    end
+    
+    if File.exist?("jesource/BINARY_INFO.txt")
+      doc.install "jesource/BINARY_INFO.txt"
+    end
+  end
+
+  test do
+    # 测试安装是否成功
+    assert_match "jesource", shell_output("#{bin}/jesource --help")
   end
 
   def caveats
